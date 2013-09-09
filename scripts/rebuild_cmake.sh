d94830a34814fb67ddcbd44d204dafe413ce842d
Florian Kruse <florian.kruse@tu-dortmund.de>
2013-09-09 14:22:08 +0200

    You asked for it, you get it! All DooSoftware now requiring C++11. Also updating the rebuild CMake scripts

--- scripts/rebuild_cmake.sh
+++ scripts/rebuild_cmake.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CMAKE_BASE_DIR="$(dirname "$DIR")"
+
 if [ -f CMakeLists.txt ]
 then 
   echo "Directory contains CMakeLists.txt. You should use a clean out of source build. Exiting."
@@ -15,10 +18,10 @@ fi
 echo "Building CMake build system for installation to $DOOCORESYS."
 if [ "$1" = "debug" ]
 then
-  cmake -DCMAKE_INSTALL_PREFIX=$DOOCORESYS -DCMAKE_BUILD_TYPE=Debug ..
+  cmake -DCMAKE_INSTALL_PREFIX=$DOOCORESYS -DCMAKE_BUILD_TYPE=Debug $CMAKE_BASE_DIR
 elif [ "$1" = "profiling" ]
 then
-  cmake -DCMAKE_INSTALL_PREFIX=$DOOCORESYS -DCMAKE_BUILD_TYPE=Profiling ..
+  cmake -DCMAKE_INSTALL_PREFIX=$DOOCORESYS -DCMAKE_BUILD_TYPE=Profiling $CMAKE_BASE_DIR
 else 
-  cmake -DCMAKE_INSTALL_PREFIX=$DOOCORESYS -DCMAKE_BUILD_TYPE=Release ..
+  cmake -DCMAKE_INSTALL_PREFIX=$DOOCORESYS -DCMAKE_BUILD_TYPE=Release $CMAKE_BASE_DIR
 fi
