afee90c5787ac0539c6c8fba3f82deb3036bcfbe
Florian Kruse <florian.kruse@tu-dortmund.de>
2014-04-14 10:56:39 +0200

    As of now DooSoftware will use the Boost version supplied with the LHCb software. Thus, I am removing the BOOST_NO_CXX11_SCOPED_ENUMS hacks

--- src/doocore/config/Summary.h
+++ src/doocore/config/Summary.h
@@ -12,9 +12,9 @@
 // from TMVA
 
 // from BOOST
-#if defined(__GNUG__) && !defined(__clang__)
-#define BOOST_NO_CXX11_SCOPED_ENUMS
-#endif
+//#if defined(__GNUG__) && !defined(__clang__)
+//#define BOOST_NO_CXX11_SCOPED_ENUMS
+//#endif
 #include <boost/filesystem.hpp>
 
 // from DooCore
