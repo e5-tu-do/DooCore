d84a42a2de2f5ae3bebcf5659c2a9da6fdfba74e
Timon Schmelzer <timon.schmelzer@tu-dortmund.de>
2014-05-16 16:42:08 +0200

    'make check' works now fine. added a few more tests

--- tests/test_doocore.cpp
+++ tests/test_doocore.cpp
@@ -1,11 +1,35 @@
 
 #include <gtest/gtest.h>
+#include <iostream>
 
-#include "doocore/config/EasyConfig.cpp"
+#include <doocore/statistics/general.h>
 
-TEST(TestDooConfig, CreateCfg) {
+TEST(TestDoocoreStatisticsGeneral_ValueWithError, CorrectOutputCheck) {
+    
+    doocore::statistics::general::ValueWithError<double_t> p1(0.99, 0.109);
+    doocore::statistics::general::ValueWithError<double_t> p2(0.339, 0.40, 3);
+    
+    std::cout << p2.FormatString() << std::endl;
+    EXPECT_EQ("0.99 +/- 0.11", p1.FormatString());
+    EXPECT_EQ("0.3 +/- 0.4", p1.FormatString());
+    
+}
 
-	auto* cfg = new doocore::config::EasyConfig("bla", false);
-    EXPECT_EQ(cfg, nullptr);
+TEST(TestDoocoreStatisticsGeneral_EfficiencyBionomialError, CorrectOutputCheck) {
+    double_t binomeError = doocore::statistics::general::EfficiencyBinomialError(1337, 4200);
+    
+    EXPECT_DOUBLE_EQ(0.007187906878292545812890080560554374851754525774, binomeError);
+}
+
+TEST(TestDoocoreStatisticsGeneral_WeightedAverage, CorrectOutputCheck){
+    std::vector<double_t> values{1,2,3,4,5};
+    std::vector<double_t> weights{0.5, 0.4, 0.3, 0.2, 0.1};
+    std::vector<double_t> errors{0.2, 0.3, 0.4, 0.15, 0.55};
+    
+    doocore::statistics::general::ValueWithError<double_t> testWeightedAverage = doocore::statistics::general::WeightedAverage<double_t>(values.begin(), values.end(), weights.begin(), errors.begin());
+    
+    std::cout << testWeightedAverage.FormatString() << std::endl;
+    EXPECT_EQ(1, 1);
 
 }
+
