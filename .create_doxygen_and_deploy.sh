2d14f7c2e0f7d22928bbe29dca4a90c50b32a2b8
Alex Birnkraut <alex.birnkraut@tu-dortmund.de>
2018-01-31 17:04:37 +0100

    first travis.yml file test

---
+++ .create_doxygen_and_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo 'Generating Doxygen code documentation...'
+doxygen doxygen.conf
+
+if [ -d "html" ] && [ -f "html/index.html" ]; then
+
+    echo 'Uploading documentation to the docs.e5.physik.tu-dortmund.de...'
+else
+    echo '' >&2
+    echo 'Warning: No documentation (html) files have been found!' >&2
+    echo 'Warning: Not going to push the documentation to docs.e5.physik.tu-dortmund.de' >&2
+    exit 1
+fi
