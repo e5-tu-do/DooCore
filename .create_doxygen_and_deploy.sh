5ebb856e6213cbc08030c0b270e61450d7da7797
Alex Birnkraut <alex.birnkraut@tu-dortmund.de>
2018-02-01 08:48:58 +0100

    playing with travis

--- .create_doxygen_and_deploy.sh
+++ .create_doxygen_and_deploy.sh
@@ -5,11 +5,14 @@ echo 'Using the following version of doxygen:'
 doxygen --version
 doxygen doxygen.conf
 
-if [ -d "html" ] && [ -f "html/index.html" ]; then
-
-    echo 'Uploading documentation to docs.e5.physik.tu-dortmund.de using curl...'
-    #first try to upload a single file
-    cd html && curl -f -u $DOCS_USER:$DOCS_PASSWORD -k -L -T index.html $DOCS_URL/index.html
+if [ -d "html" ] && [ -f "html/index.html" ];
+then
+	# this line can be delted when the dns entry is available
+	echo "$DOCS_HOSTS" >> /etc/hosts
+	cat /etc/hosts
+	echo 'Uploading documentation to docs.e5.physik.tu-dortmund.de using curl...'
+	#first try to upload a single file
+	cd html && curl -f -u $DOCS_USER:$DOCS_PASSWORD -k -L -T index.html $DOCS_URL/index.html
 
 else
     echo '' >&2
