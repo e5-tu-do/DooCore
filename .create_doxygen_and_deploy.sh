38a8b50f8662ce274744dca6e75db36def3481ed
Alex Birnkraut <alex.birnkraut@tu-dortmund.de>
2018-02-01 07:54:36 +0100

    playing with travis

--- .create_doxygen_and_deploy.sh
+++ .create_doxygen_and_deploy.sh
@@ -4,10 +4,11 @@ echo 'Generating Doxygen code documentation...'
 doxygen --version
 apt-cache policy doxygen
 doxygen doxygen.conf
+echo 'This is the encrypted user: $doocore-USER'
 
 if [ -d "html" ] && [ -f "html/index.html" ]; then
 
-    echo 'Uploading documentation to the docs.e5.physik.tu-dortmund.de...'
+    echo 'Uploading documentation to docs.e5.physik.tu-dortmund.de...'
 else
     echo '' >&2
     echo 'Warning: No documentation (html) files have been found!' >&2
